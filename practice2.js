359b8cece0d5d1bae35e7cc9742782f8c82ba7d8
Masaki <mike18goonerrr@gmail.com>
2022-04-02 23:13:11 +0900

    非同期処理練習。1秒ごとに値をカウントアップ

---
+++ practice2.js
@@ -0,0 +1,35 @@
+//自分で解いてみた（間違えた）
+// const sleep = new Promise((callback, val) => {
+//     setTimeout(() => {
+//         console.log(val++);
+//         resolve(val);
+//     }, 1000);
+// })
+
+/**
+ * sleepには数値を渡す
+ * Promiseの引数はresolve or reject
+ * 今回はrejectしないのでresolveだけ渡すようにする。
+ * setTimeoutで1秒ごとにvalをカウントアップして出力するようにする
+ * */
+
+const sleep = (val) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(val++);
+            resolve(val);
+        }, 1000);
+    })
+}
+
+//関数呼び出し
+//sleep(0).then(sleep).then(sleep).then(sleep);
+
+sleep(0)
+    .then((val) => sleep(val))
+    .then((val) => sleep(val))
+    .then((val) => sleep(val))
+    .then((val) => sleep(val))
+    .then((val) => sleep(val));
+
+//sleep(0)のあとで.thenができるのは関数sleepがPromiseオブジェクトをreturnしているから
