597be9edb8fc821205fc56519eae557c5197daf5
Masaki <mike18goonerrr@gmail.com>
2022-04-03 23:29:27 +0900

    Promise.all挙動確認

--- practice3.js
+++ practice3.js
@@ -1,2 +1,10 @@
-console.log('HELLO GIT');
+const sleep = (val) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(val++);
+            resolve(val);
+        }, 1000);
+    });
+}
 
+Promise.all([sleep(0), sleep(1), sleep(2)]).then(() => console.log('end'));
