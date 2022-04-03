6d74ec9071c758abe3b2ddf17d4efeec27a2e3d9
Masaki <mike18goonerrr@gmail.com>
2022-04-03 23:50:38 +0900

    Async Await practice

---
+++ practice4.js
@@ -0,0 +1,38 @@
+/** Asyncを使って宣言された関数はPromiseオブジェクトを返す→thenを繋げられる
+* AwaitはPromiseを返す関数の非同期処理が終わるまでまつ
+* 関数ないでawaitが記述されている場合、asyncが関数の先頭にないとエラーとなる
+*
+ *  以下を簡略化して書く
+* const sleep = (val) => {
+*     return new Promise((resolve) => {
+*         setTimeout(() => {
+*             console.log(val++);
+*             resolve(val);
+*         }, 1000);
+*     })
+* }
+* sleep(0).then(sleep).then(sleep).then(sleep);
+ */
+
+const sleep = (val) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(val++)
+            resolve(val);
+        }, 1000);
+    });
+};
+
+async function init() {
+    let val = await sleep(0);
+    val = await sleep(val);
+    val = await sleep(val);
+    val = await sleep(val);
+    val = await sleep(val);
+    return val;
+};
+
+init().then((val) => console.log(`val: ${val}`));
+
+
+
