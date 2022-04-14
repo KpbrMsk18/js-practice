7ecba60e7574fafc7abd40a9012495685e09baf7
Masaki <mike18goonerrr@gmail.com>
2022-04-15 00:04:10 +0900

    Revert "revert前"
    This reverts commit 5eff0857c980dddeaf7daa18f208c375361d2157.

--- practice6.js
+++
@@ -1,9 +0,0 @@
-
-new Promise ((resolve, reject) => {
-    console.log('test');
-    setTimeout(() => {
-        resolve('hoge');
-    }, 1000)
-}).then((data) => {
-    console.log(`then - ${data}`);
-})
